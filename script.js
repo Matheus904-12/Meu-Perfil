37a18fb9494789cb3d029a56364caae72195c17f
Matheus904-12 <67837282+Matheus904-12@users.noreply.github.com>
2023-09-26 17:06:49 -0300

    Até casa

--- script.js
+++ script.js
@@ -2,44 +2,44 @@ document.addEventListener('DOMContentLoaded', function() {
     const repositories = [
         {
             name: 'Demais.com',
-            description: 'Fã-Page do Prona',
-            coverImage: 'projeto1.jpg',
+            description: 'Teste da Fã-Page do Prona',
+            coverImage: 'Demais.com.jpg',
             languages: ['HTML', 'CSS', 'JS']
         },
         {
             name: 'Demais.com-main',
-            description: '2ª Fã-Page do Prona',
-            coverImage: 'projeto2.jpg',
+            description: 'Página definitivan da Fã-Page do Prona',
+            coverImage: 'Demais.com.jpg',
             languages: ['HTML', 'CSS', 'JS']
         },
         {
             name: 'Figma',
             description: 'Figma para HTML',
-            coverImage: 'projeto2.jpg',
+            coverImage: 'Figma_to_html_1.png',
             languages: ['HTML', 'CSS', 'JS']
         },
         {
             name: 'X-Men',
             description: 'Seleção de personagens do X-Men',
-            coverImage: 'projeto2.jpg',
+            coverImage: 'xmen.png',
             languages: ['HTML', 'CSS', 'JS']
         },
         {
             name: 'Poke',
             description: 'Seleção de personagens do Pokémon Go',
-            coverImage: 'projeto2.jpg',
+            coverImage: 'pokemon-slider.png',
             languages: ['HTML', 'CSS', 'JS']
         },
         {
             name: 'Mario',
             description: 'Divulgação do filme Mario',
-            coverImage: 'projeto2.jpg',
+            coverImage: 'css-pixel-art.png',
             languages: ['HTML', 'CSS', 'JS']
         },
         {
             name: 'Marvel',
             description: 'Seleção de personagens da Marvel',
-            coverImage: 'projeto2.jpg',
+            coverImage: 'marvel.jpg',
             languages: ['HTML', 'CSS', 'JS']
         },
         {
@@ -87,49 +87,49 @@ document.addEventListener('DOMContentLoaded', function() {
         {
             name: 'Projeto-OnePage',
             description: 'Comida Caseira',
-            coverImage: 'projeto2.jpg',
+            coverImage: 'restaurante.jpeg',
             languages: ['HTML', 'CSS', 'JS']
         },
         {
             name: 'Namorar-',
             description: 'Pedido de Namoro',
-            coverImage: 'projeto2.jpg',
+            coverImage: 'namorar.jpg',
             languages: ['HTML', 'CSS', 'JS']
         },
         {
             name: 'Pedra-Papel',
             description: 'Pedra, Papel e Tesoura',
-            coverImage: 'projeto2.jpg',
+            coverImage: 'jokenpo.png',
             languages: ['HTML', 'CSS', 'JS']
         },
         {
             name: 'Meu-bingo',
             description: 'Jogo do Bingo',
-            coverImage: 'projeto2.jpg',
+            coverImage: 'bingo.jpg',
             languages: ['HTML', 'CSS', 'JS']
         },
         {
             name: 'sorte',
             description: 'Sorteio de Números e Nomes',
-            coverImage: 'projeto2.jpg',
+            coverImage: 'sorteio.jpg',
             languages: ['HTML', 'CSS', 'JS']
         },
         {
             name: 'JS-BIN',
             description: 'Cadastro de Livros',
-            coverImage: 'projeto2.jpg',
+            coverImage: 'bibliotecas.png',
             languages: ['HTML', 'CSS', 'JS']
         },
         {
             name: 'Quizz',
             description: 'Quizz de Perguntas',
-            coverImage: 'projeto2.jpg',
+            coverImage: 'a-guide-to-html-css-forms-no-hacks.jpg',
             languages: ['HTML', 'CSS', 'JS']
         },
         {
             name: 'JAVASCRIPT',
             description: 'Formulário Inicial',
-            coverImage: 'projeto2.jpg',
+            coverImage: 'a-guide-to-html-css-forms-no-hacks.jpg',
             languages: ['HTML', 'CSS', 'JS']
         },
         {
@@ -141,13 +141,13 @@ document.addEventListener('DOMContentLoaded', function() {
         {
             name: 'Jogo-Da-Cobrinha',
             description: 'Jogo Do Nokia',
-            coverImage: 'projeto2.jpg',
+            coverImage: 'cobrinha.png',
             languages: ['HTML', 'CSS', 'JS']
         },
         {
             name: 'Desenho',
             description: 'Paint 2D',
-            coverImage: 'projeto2.jpg',
+            coverImage: '20151220031246_canvas.png',
             languages: ['HTML', 'CSS', 'JS']
         },
         {
@@ -157,8 +157,8 @@ document.addEventListener('DOMContentLoaded', function() {
             languages: ['HTML', 'CSS', 'JS']
         },
         {
-            name: 'Não Definido',
-            description: 'Descrição do Projeto 2',
+            name: 'OO',
+            description: 'Calculadora',
             coverImage: 'projeto2.jpg',
             languages: ['HTML', 'CSS', 'JS']
         },
